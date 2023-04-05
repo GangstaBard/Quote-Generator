9d4db87d3ed8c959919d4bd1434b5a7528dedc35
Aleospen <tsarexecutor@gmail.com>
2023-04-05 21:30:14 +0300

    first commit

---
+++ quote-generator/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <header>
+    <h1>Best quotes of all times!</h1>
+  </header>
+  <main><QuoteGenerator /></main>
+</template>
+
+<script>
+import QuoteGenerator from "@/components/QuoteGenerator.vue";
+
+export default {
+  name: "App",
+  components: {
+    QuoteGenerator,
+  },
+};
+</script>
+
+<style lang="scss">
+body {
+  background-image: linear-gradient(to right, #a9f1df, #ffbbbb);
+}
+
+main {
+  height: 80vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ quote-generator/src/components/QuoteGenerator.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="quote">
+    <button class="quote__btn">Get new quote</button>
+    <p class="quote__text">
+      <i
+        >"Success is not final; failure is not fatal: It is the courage to
+        continue that counts."</i
+      >
+    </p>
+  </section>
+</template>
+
+<style lang="scss">
+.quote {
+  &__text {
+  }
+
+  &__btn {
+    padding: 1%;
+    border-radius: 1em;
+    background-color: red;
+    color: #fff;
+    font-weight: 600;
+    transition: all 0.3s;
+
+    &:hover {
+      transform: scale(1.2);
+    }
+  }
+}
+</style>
+
+<script>
+export default { name: "QuoteGenerator" };
+</script>
