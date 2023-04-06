e6a0db1ff69b7a25a45b58823959ea0a9f2f50aa
Aleospen <tsarexecutor@gmail.com>
2023-04-06 15:28:47 +0300

    adding choosing the category for quotes

--- quote-generator/src/components/QuoteGenerator.vue
+++ quote-generator/src/components/QuoteGenerator.vue
@@ -1,5 +1,16 @@
 <template>
   <section class="quote">
+    <form>
+      <h3>You can choose a category for quotes!</h3>
+      <input
+        v-model="currentCategory"
+        id="category"
+        type="text"
+        placeholder="category"
+      />
+      <p>Current category: {{ currentCategory }}</p>
+    </form>
+
     <button @click="getQuote" class="quote__btn">Get new quote</button>
     <p class="quote__text">
       <i>{{ currentQuote }}</i>
@@ -8,9 +19,18 @@
 </template>
 
 <style lang="scss">
+form {
+  margin-bottom: 8vh;
+}
+
+input {
+  padding: 0.5vw;
+  border-radius: 1em;
+}
+
 .quote {
   &__text {
-    font-size: larger;
+    margin: 1em;
   }
   &__btn {
     padding: 10px;
@@ -38,11 +58,20 @@ export default {
   },
   methods: {
     async getQuote() {
-      const response = await fetch('https://api.quotable.io/random')
-      const quote = await response.json()
-      this.currentQuote = quote.content
+      if (this.currentCategory != undefined) {
+        const response = await fetch(
+          'https://api.quotable.io/random?tags=' + this.currentCategory
+        )
+        const quote = await response.json()
+        this.currentQuote = quote.content
+      } else {
+        const response = await fetch('https://api.quotable.io/random')
+        const quote = await response.json()
+        this.currentQuote = quote.content
+      }
     },
   },
+
   mounted() {
     this.getQuote()
   },
