36b79a18144429a198657ed147832b6a444a83ab
Aleospen <tsarexecutor@gmail.com>
2023-04-06 11:21:59 +0300

    generation

--- quote-generator/src/App.vue
+++ quote-generator/src/App.vue
@@ -6,14 +6,14 @@
 </template>
 
 <script>
-import QuoteGenerator from "@/components/QuoteGenerator.vue";
+import QuoteGenerator from '@/components/QuoteGenerator.vue'
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
     QuoteGenerator,
   },
-};
+}
 </script>
 
 <style lang="scss">
--- quote-generator/src/components/QuoteGenerator.vue
+++ quote-generator/src/components/QuoteGenerator.vue
@@ -1,11 +1,8 @@
 <template>
   <section class="quote">
-    <button class="quote__btn">Get new quote</button>
+    <button @click="getQuote" class="quote__btn">Get new quote</button>
     <p class="quote__text">
-      <i
-        >"Success is not final; failure is not fatal: It is the courage to
-        continue that counts."</i
-      >
+      <i>{{ currentQuote }}</i>
     </p>
   </section>
 </template>
@@ -13,15 +10,16 @@
 <style lang="scss">
 .quote {
   &__text {
+    font-size: larger;
   }
-
   &__btn {
-    padding: 1%;
-    border-radius: 1em;
-    background-color: red;
-    color: #fff;
+    padding: 10px;
+    border-radius: 2em;
+    background-image: linear-gradient(to right, #6ec0ac, #bd6a6a);
+
+    color: #f0ebeb;
     font-weight: 600;
-    transition: all 0.3s;
+    transition: all 0.4s;
 
     &:hover {
       transform: scale(1.2);
@@ -31,5 +29,22 @@
 </style>
 
 <script>
-export default { name: "QuoteGenerator" };
+export default {
+  data() {
+    return {
+      currentQuote: '',
+      currentCategory: '',
+    }
+  },
+  methods: {
+    async getQuote() {
+      const response = await fetch('https://api.quotable.io/random')
+      const quote = await response.json()
+      this.currentQuote = quote.content
+    },
+  },
+  mounted() {
+    this.getQuote()
+  },
+}
 </script>
